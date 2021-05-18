c324ca993f1939d522dc4b778a9f7c98c87aaeaa
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-05-18 20:22:53 +0200

    main

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,60 @@
-$(document).ready(function () {
+$(document).ready(function() {
     selectUser()
-    $(document).on('#action', 'click', function(){
-        alert("Clicked")
+    $('#action').click(function() {
+        let username = $('#username').val();
+        let fullname = $('#fullname').val();
+
+        if (username != '' && fullname != '') {
+            let action = "insert";
+            $.ajax({
+                url: './config/config.php',
+                method: 'POST',
+                data: {
+                    action,
+                    username,
+                    fullname
+                },
+                success: function(data) {
+                    selectUser()
+                }
+            })
+            alert(username + ' ' + fullname)
+        } else {
+            alert("Some fields are empty")
+        }
+    })
+    $(document).on('click', '.update', function() {
+        let id = $(this).attr('id')
+        let action = 'getUser'
+
+        $.ajax({
+            url: './config/config.php',
+            method: 'POST',
+            data: {
+                action,
+                id
+            },
+            dataType: 'json',
+            success: function(data) {
+                $('#id_user').val(id)
+                $('#username').val(data.username)
+                $('#fullname').val(data.fullname)
+                $('#action').text('Update ' + data.username)
+            }
+        })
     })
 })
-function selectUser(){
+
+function selectUser() {
     let action = "select"
     $.ajax({
         url: './config/config.php',
         method: 'POST',
-        data: {action},
-        success: function(data){
+        data: {
+            action
+        },
+        success: function(data) {
             $('#result').html(data)
-        }
-    })
-}
-
-function insertUser(){
-    let action = "insert"
-    $.ajax({
-        url: '',
-        method: '',
-        data: {action, username, fullname},
-        success: function(data){
-            selectUser()
         }
     })
 }
