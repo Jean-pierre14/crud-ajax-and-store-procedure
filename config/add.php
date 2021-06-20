cdd8f04ce59b2c8dc097b1bbd7a80acc49fcb459
Jean-pierre14 <chiruzabisimwa@gmail.com>
2021-06-20 19:38:51 +0200

    Ad more once

--- config/add.php
+++ config/add.php
@@ -4,16 +4,18 @@ $con = mysqli_connect("localhost", "root", "", "crudajax") or die("couldn't conn
 
 $num = count($_POST['name']);
 
-if($num > 1){
-    for($i = 0; $i < $num; $i++){
-        if(trim($_POST['name'][$i]) != ''){
-            $sql = mysqli_query($con, "INSERT INTO children_tb(name) VALUE('".mysqli_real_escape_string($con, $_POST['name'][$i]) ."')");
-            if($sql) {
+if ($num > 1) {
+    for ($i = 0; $i < $num; $i++) {
+
+        if (trim($_POST['name'][$i]) != '') {
+
+            $sql = mysqli_query($con, "INSERT INTO children_tb(`name`) VALUE('" . mysqli_real_escape_string($con, $_POST['name'][$i]) . "')");
+            if ($sql) {
                 $out = '<p>Registerd</p>';
             }
         }
     }
-}else{
+} else {
     $out = '<p>Please entre the name</p>';
 }
 
