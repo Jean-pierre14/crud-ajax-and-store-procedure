c92e0811e5a0f1a596a9c457827aea2d6f025645
User <chiruzabisimwa@gmail.com>
2021-07-13 12:40:07 +0200

    Attendance List done

--- config/attendance.php
+++ config/attendance.php
@@ -1,11 +1,12 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "crudajax") or die("Can't be connect to this DB");
-$today = date('Y-m-d');
+
 
 $output = '';
+$dateNow = date('Y-m-d');
 if(isset($_POST['action'])){
     if($_POST['action'] == 'attended'){
-        
+        $today = date('Y-m-d');
         $sql = mysqli_query($con, "SELECT * FROM users INNER JOIN attendance ON users.id=attendance.user_id WHERE attended = '$today'");
         if(@mysqli_num_rows($sql) > 0){
             $output .= '<ul class="list-group">';
@@ -38,64 +39,80 @@ if(isset($_POST['action'])){
         print $output;
     }
     if($_POST['action'] == 'select'){
-        $dateNow = date('Y-m-d');
+        
         $sql = mysqli_query($con, "SELECT * FROM users");
         if(@mysqli_num_rows($sql) > 0){
             $output .= '<ul class="list-group">';
             while($row = mysqli_fetch_array($sql)):
                 if($row['today'] == $dateNow){
-                    if($row['today'] == ''){
-                        $output .= '
-                        <li class="list-group-item d-flex justify-content-between align-item-center">
-                        <span>'.$row['fullname'].'</span>
-                        <span class="btn-group">
-                            <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
-                            <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
-                        </span>
-                        </li>';
-                    }elseif($row['today'] == 'no'){
-                        $output .= '
-                    <li class="list-group-item list-group-item-danger d-flex justify-content-between align-item-center">
+                    $output .= '
+                    <li class="list-group-item d-flex list-group-item-success justify-content-between align-item-center">
+                    <span>'.$row['fullname'].'</span>
+                    <span>
+                        <button class="btn btn-sm btn-primary" type="button" id="'.$row['id'].'">See</button>
+                    </span>
+                    </li>';
+                }elseif($row['today'] == 'no'){
+                    $output .= '
+                    <li class="list-group-item d-flex list-group-item-danger justify-content-between align-item-center">
+                    <span>'.$row['fullname'].' '.$dateNow.'</span>
+                    <span>
+                        <button class="btn btn-sm btn-secondary" type="button" id="'.$row['id'].'">See</button>
+                    </span>
+                    </li>';
+                }else{
+                    $output .= '
+                    <li class="list-group-item d-flex justify-content-between align-item-center">
                     <span>'.$row['fullname'].'</span>
                     <span class="btn-group">
                         <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
                         <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
                     </span>
                     </li>';
-                    }else{
-                        $output .= '
-                    <li class="list-group-item list-group-item-primary d-flex justify-content-between align-item-center">
-                    <span>'.$row['fullname'].'</span>
-                    ';
-                    if($row['today'] == ''){
-                        $output .= '
-                        <span class="btn-group">
-                            <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
-                            <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
-                        </span>
-                        ';
-                    }elseif($row['today'] == 'no'){
-                        $output .= '
-                            <button type="button" class="btn btn-sm btn-secondary" id="'.$row['id'].'">See</button>
-                        ';
-                    }else{
-                        $output .= '
-                        <button type="button" class="btn btn-sm btn-primary" id="'.$row['id'].'">See</button>
-                        ';
-                    }
-                    $output.= '
-                    </li>';
-                    }
-                }else{
-                    $output .= '
-                        <li class="list-group-item d-flex justify-content-between align-item-center">
-                        <span>'.$row['fullname'].'</span>
-                        <span class="btn-group">
-                            <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
-                            <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
-                        </span>
-                        </li>';
                 }
+                // if($row['today'] == ''){
+                //     $output .= '
+                //     <li class="list-group-item d-flex justify-content-between align-item-center">
+                //     <span>'.$row['fullname'].'</span>
+                //     <span class="btn-group">
+                //         <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
+                //         <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
+                //     </span>
+                //     </li>';
+                // }elseif($row['today'] == 'no'){
+                //     $output .= '
+                //     <li class="list-group-item list-group-item-danger d-flex justify-content-between align-item-center">
+                //     <span>'.$row['fullname'].'</span>
+                //     <span class="btn-group">
+                //         <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
+                //         <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
+                //     </span>
+                //     </li>';
+                // }else{
+                //     $output .= '
+                //     <li class="list-group-item list-group-item-primary d-flex justify-content-between align-item-center">
+                //     <span>'.$row['fullname'].'</span>
+                //     ';
+                
+                //     if($row['today'] == ''){
+                //         $output .= '
+                //         <span class="btn-group">
+                //             <button type="button" class="btn btn-sm btn-primary yes" id="'.$row['id'].'">Yes</button>
+                //             <button type="button" class="btn btn-sm btn-warning no" id="'.$row['id'].'">No</button>
+                //         </span>
+                //         ';
+                //     }elseif($row['today'] == 'no'){
+                //         $output .= '
+                //             <button type="button" class="btn btn-sm btn-secondary" id="'.$row['id'].'">See</button>
+                //         ';
+                //     }else{
+                //         $output .= '
+                //         <button type="button" class="btn btn-sm btn-primary" id="'.$row['id'].'">See</button>
+                //         ';
+                //     }
+                // $output.= '
+                // </li>';
+                // }
             endwhile;
             $output .= '</ul>';
         }else{
@@ -107,7 +124,7 @@ if(isset($_POST['action'])){
         $id = $_POST['id'];
         $sql = mysqli_query($con, "INSERT INTO attendance(`user_id`, attended) VALUES('$id', now())");
         if($sql){
-            mysqli_query($con, "UPDATE users SET today = now() WHERE id = $id");
+            mysqli_query($con, "UPDATE users SET today = '$dateNow' WHERE id = $id");
             print 'success';
         }else{
             print 'Error';
